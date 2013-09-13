a034a631a4134596724efc024f7d5f53a46ad50a
Ryan Huber <rhuber@gmail.com>
2013-09-13 16:34:11 -0700

    baby steps - connect to socket to send some infoz

--- proxy.js
+++ proxy.js
@@ -2,14 +2,32 @@
 
 var http = require('http');
 var httpProxy = require('http-proxy');
-
 var net = require('net');
 
+//CONSTANTS
+var UPSTREAM_LOGSERVER = process.argv[2];
+
+var upstreamConnection;
+
+
 setInterval(function() {
-  console.log('hi')
-  },5000);
+  if (!upstreamConnection) {
+//  try {
+    upstreamConnection = net.connect({port: 8222}, function() {
+    console.log("Connected");
+//      console.log('connected')
+      });
+  };
+//  } catch (e) {console.log('hi')};
+},1000);
+
+setInterval(function() {
+  upstreamConnection.write("TEST\n");
+
+},2000);
+
 
 
 http.createServer(function(req, res) {
   res.end('hello world\n');
-  }).listen(8080);
+}).listen(8080);
