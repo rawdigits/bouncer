8918d2259e176ebec386319dc38de14788e7a2aa
Ryan Huber <rhuber@gmail.com>
2013-09-13 16:54:05 -0700

    yay for input

--- proxy.js
+++ proxy.js
@@ -10,19 +10,32 @@ var UPSTREAM_LOGSERVER = process.argv[2];
 var upstreamConnection;
 
 
+function commandDo(cmd) {
+  cmd = cmd.toString().trim();
+
+  console.log(cmd);
+}
+
+
 setInterval(function() {
   if (!upstreamConnection) {
-    upstreamConnection = net.connect({port: 8222})
+    upstreamConnection = net.connect({port: 8222});
+    upstreamConnection.on('data', function(data) {
+      commandDo(data);
+    });
     upstreamConnection.on('error', function () {
       upstreamConnection = null;
-    })
+    });
   };
 },1000);
 
 setInterval(function() {
-  upstreamConnection.write("TEST\n");
+  try {
+    upstreamConnection.write("TEST\n");
+  } catch (e) {};
 
 },2000);
+
 
 
 
