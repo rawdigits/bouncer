e412b1fa4f8689f2c95037b628082c15da103da6
Ryan Huber <rhuber@gmail.com>
2013-09-13 16:04:18 -0700

    htttttteeeeeeeppp

--- proxy.js
+++ proxy.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
 
+var http = require('http');
+var httpProxy = require('http-proxy');
 
+var net = require('net');
+
+setInterval(function() {
+  console.log('hi')
+  },5000);
+
+
+http.createServer(function(req, res) {
+  res.end('hello world\n');
+  }).listen(8080);
