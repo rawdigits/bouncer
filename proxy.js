344c5c47d4e69d6c0850c05a1e77520665a3df3c
Ryan Huber <rhuber@gmail.com>
2013-09-16 15:08:57 -0700

    cleanup and more removal of debug

--- proxy.js
+++ proxy.js
@@ -43,8 +43,6 @@ function buildMessage(req, uuid) {
 function checkRequest(req) {
   if (req.socket.remoteAddress in assholes) {
     if (assholes[req.socket.remoteAddress] > new Date().getTime()) {
-      //console.log(assholes[req.socket.remoteAddress]);
-      //console.log(new Date().getTime());
       req.connection.end();
       return false;
     } else {
@@ -54,21 +52,19 @@ function checkRequest(req) {
   } else {
     return true;
   }
-  //console.log(req.socket.remoteAddress);
 }
 
 //This connects to the aggregation server and accepts upstream commands.
 setInterval(function() {
-  if (!upstreamConnection) {
-    console.log('creating upstream');
+  if (!upstreamConnection || upstreamConnection == null) {
     upstreamConnection = net.connect({host: UPSTREAM_LOGSERVER, port: 5555})
     //connect to aggregator in "server" mode
     upstreamConnection.write('S');
-    upstreamConnection.once('data', function(data) {
+    upstreamConnection.on('data', function(data) {
       commandDo(data);
     });
     //destroys upstream if the connection is dead
-    upstreamConnection.once('error', function () {
+    upstreamConnection.on('error', function () {
       return upstreamConnection = null
     });
   };
@@ -76,7 +72,6 @@ setInterval(function() {
 
 setInterval(function() {
   //try {
-//    console.log(proxyServer.getConnections());
     //upstreamConnection.write("TEST\n");
   //} catch (e) {};
 },1000);
