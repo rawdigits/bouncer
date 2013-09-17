9d760ea11fe252f81e885fb6fc98b78328082dfc
Ryan Huber <rhuber@gmail.com>
2013-09-16 17:55:48 -0700

    remove console log messages, cleanup

--- aggregator.js
+++ aggregator.js
@@ -4,40 +4,34 @@ var net = require('net');
 
 clients = []
 servers = []
+commandOnlyClients = []
 
-//console.log(clients);
 
 function bye(c) {
  if (clients.indexOf(c) > -1) {
     index = clients.indexOf(c);
     clients.splice(index, 1);
-    //console.log("Clients: " + clients.length);
   } else if (servers.indexOf(c) > -1) {
     index = servers.indexOf(c);
     servers.splice(index, 1);
-    //console.log("Servers: " + servers.length);
   }
 };
 
 server = net.createServer(function(c) {
   oldData = '';
   c.on('data', function(data) {
-    //console.log(data.toString().trim());
     if (data.toString().trim() == 'S') {
       servers.push(c);
-      //console.log(servers.length);
     } else if (data.toString().trim() == 'C') {
       clients.push(c);
-      //console.log(clients.length);
+    } else if (data.toString().trim() == 'CO') {
+      console.log('commandy');
+      commandOnlyClients.push(c);
     } else {
       if (data.toString()[data.length-1] == '\n') {
+        //assembles data into full size chunks
         allData = oldData + data;
-        if (oldData != '') {
-          console.log("fixed some data");
-          console.log(allData.length);
-        }
         oldData = '';
-        //console.log ('complete data ' + data.length );
         if (servers.indexOf(c) > -1) {
           clients.forEach(function (sock) {
             sock.write(allData);
@@ -47,12 +41,10 @@ server = net.createServer(function(c) {
             sock.write(allData);
           });
         } else {
-          c.write('Servers: ' + servers.length + "\nClients: " + clients.length + "\n");
+          c.write('Servers: ' + servers.length + "\nClients: " + clients.length + "Command only Clients: " + commandOnlyClients.lentgh + "\n");
         };
       } else {
         oldData = oldData + data
-        //console.log ('incomplete data ' + data.length );
-        //console.log(data.toString());
       };
     };
   });
