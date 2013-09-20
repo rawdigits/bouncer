12440bd852935f56fec9b160df67d1b87e9254a8
rhuber <rhuber@fjdev.rednu.com>
2013-09-20 12:45:07 -0700

    remove debug stuff

--- example/shared.py
+++ example/shared.py
@@ -37,36 +37,11 @@ class AggregatorConnector:
         good = data.split('\n')
         records.append(good[0])
         self.previous_data = good[1]
-      #print "ONE"
     else:
       self.previous_data = data
-      #print "EXTRA"
-      #print data
     return records
 
-#    if data[-1] == "\n":
-#      return [d]
-#    #if data[0] == '{' and data[-1] == "\n":
-#      if data[-1] == "\n":
-#      if data.count("\n") > 1:
-#      else:
-#        return [data.rstrip('\n')]
-#    else:
-#      return []
-
   def json_read(self):
-    data = self.raw_read()
-    try:
-      return [json.loads(line) for line in data]
-    except:
-      print data
-#    if data != []:
-    #  try:
-#    else:
-#      return []
-    #  except:
-    #    print data
-    #    return []
-
-
+    #data = self.raw_read()
+    return [json.loads(line) for line in self.raw_read()]
 
