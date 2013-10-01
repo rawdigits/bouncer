f078f59f5bf64711620e1fc138987c45882abcd5
rhuber <rhuber@fjdev.rednu.com>
2013-10-01 14:47:03 -0700

    add granularity to redis loaders

--- example/redis-loaders/host_and_url.py
+++ example/redis-loaders/host_and_url.py
@@ -7,14 +7,16 @@ import shared
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 b = shared.BatchCounter(5,20000)
+agg = shared.AggregatorConnector()
 
 metric = {}
+granularity=60
 
 def process_data(data):
   global metric
   #print data
   if data['type'] == 'request':
-    time = data['time']/1000
+    time = ((data['time']/1000)/granularity)*granularity
     TIME_LENGTH=10
 
     #don't care about params
@@ -32,9 +34,6 @@ def process_data(data):
         r.zincrby(time+"-hosturls", hosturl, v)
 
       metric.clear()
-
-
-agg = shared.AggregatorConnector()
 
 while True:
   for d in agg.json_read():
--- example/redis-loaders/url.py
+++ example/redis-loaders/url.py
@@ -7,14 +7,16 @@ import shared
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 b = shared.BatchCounter(5,20000)
+agg = shared.AggregatorConnector()
 
 metric = {}
+granularity = 60
 
 def process_data(data):
   global metric
   #print data
   if data['type'] == 'request':
-    time = data['time']/1000
+    time = ((data['time']/1000)/granularity)*granularity
     TIME_LENGTH=10
 
     #don't care about params
@@ -34,7 +36,6 @@ def process_data(data):
       metric.clear()
 
 
-agg = shared.AggregatorConnector()
 
 while True:
   for d in agg.json_read():
